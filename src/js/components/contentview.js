b18c3dc4b81a74a6c1d2e7707f1e64fdc1c33b21
Ashley Chapman <xpsx.fan@googlemail.com>
2017-09-01 16:02:54 +0100

    Added that starts of the Content View Implementation
    Added that starts of the Content View Implementation, which will be the bases of where 80 / 90% of the content will be placed.

--- src/js/components/contentview.js
+++ src/js/components/contentview.js
@@ -3,8 +3,13 @@ import React from "react";
 export default class ContentView extends React.Component {
     render() {
         return (
-            <div>
+            <div className="content">
+                <div className="headerCarousel">
+                    <h1>Hello, World</h1>
+                </div>
+                <div className="contentBody">
 
+                </div>
             </div>
         );
     }
