a575f27587d05efdbc83a151ce0b3ec2f241f93d
Ashley Chapman <xpsx.fan@googlemail.com>
2017-09-03 00:26:43 +0100

    Footer Now Is 100% Implemented with Responsive Features

--- src/js/components/footerbar.js
+++ src/js/components/footerbar.js
@@ -8,29 +8,31 @@ export default class FooterBar extends React.Component {
         return (
             <div className="footer">
                 <div className="footercontent">
-                    <div className="footercontent-contactdetails">
-                        <div className="footercontent-addressdetails">
-                            <h3><strong>CHALET LA GIETTAZ</strong></h3>
-                            <h4 className="footerdetailheader">Route du Col des Aravis</h4>
-                            <h4 className="footerdetailheader">73590 La Giettaz</h4>
-                            <h4 className="footerdetailheader">FRANCE</h4>
-                        </div>
-                        <div className="footercontent-telandemaildetails">
-                            <h3><strong>TEL</strong></h3>
-                            <h4 className="footerdetailheader">+33 (0)6 73 39 10 73</h4>
-                            <h3><strong>Email</strong></h3>
-                            <a className="hyperlink" href="#">reservations@chalet-la-giettaz.com</a>
-                        </div>
-                        <div className="footercontent-socialdetails">
-                            <div className="footercontent-newsletter">
-                                <img className="newsletter-icon" src="/images/badges/Mail.png"/>
-                                <h4 className="footerdetailheader newsletter-title"> Get the Chalet la Ciettaz Newsletter</h4>
+                    <div className="footercontent-contactdetails-container">
+                        <div className="footercontent-contactdetails">
+                            <div className="footercontent-addressdetails">
+                                <h3><strong>CHALET LA GIETTAZ</strong></h3>
+                                <h4 className="footerdetailheader">Route du Col des Aravis</h4>
+                                <h4 className="footerdetailheader">73590 La Giettaz</h4>
+                                <h4 className="footerdetailheader">FRANCE</h4>
                             </div>
-                            <div className="footercontent-sociallinks">
-                                <FooterBarSocialItem icon="/images/badges/social_instagram.png" link=""/>
-                                <FooterBarSocialItem icon="/images/badges/social_twitter.png" link=""/>
-                                <FooterBarSocialItem icon="/images/badges/social_facebook.png" link=""/>
-                                <FooterBarSocialItem icon="/images/badges/social_tripadvisor.png" link=""/>
+                            <div className="footercontent-telandemaildetails">
+                                <h3><strong>TEL</strong></h3>
+                                <h4 className="footerdetailheader">+33 (0)6 73 39 10 73</h4>
+                                <h3><strong>Email</strong></h3>
+                                <a className="hyperlink" href="#">reservations@chalet-la-giettaz.com</a>
+                            </div>
+                            <div className="footercontent-socialdetails">
+                                <div className="footercontent-newsletter">
+                                    <img className="newsletter-icon" src="/images/badges/Mail.png"/>
+                                    <h4 className="footerdetailheader newsletter-title"> Get the Chalet la Ciettaz Newsletter</h4>
+                                </div>
+                                <div className="footercontent-sociallinks">
+                                    <FooterBarSocialItem icon="/images/badges/social_instagram.png" link=""/>
+                                    <FooterBarSocialItem icon="/images/badges/social_twitter.png" link=""/>
+                                    <FooterBarSocialItem icon="/images/badges/social_facebook.png" link=""/>
+                                    <FooterBarSocialItem icon="/images/badges/social_tripadvisor.png" link=""/>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -39,7 +41,7 @@ export default class FooterBar extends React.Component {
                             <h6>La Giettaz Properties SARL 2017</h6>
                         </div>
                         <div className="footercontent-copyrightrightcontent">
-                            <h6>Site By <strong>RADIATOR</strong></h6>
+                            <h6>Site By <strong className="footercontent-companyname">RADIATOR</strong></h6>
                         </div>
                     </div>
                 </div>
